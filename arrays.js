0dcb0a53f75b8a1fbab1b6c78c9c6427a948360f
alyxpais <apaisner3@gatech.edu>
2017-08-28 20:13:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,5 +5,13 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element)
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray() {
   return array.push(element)
 }
